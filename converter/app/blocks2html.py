c19fd26134144fece319f998ad5d2e4943bd3600
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2024-02-01 20:22:35 +0200

    wip on convert blocks to html

--- converter/app/blocks2html.py
+++ converter/app/blocks2html.py
@@ -3,16 +3,15 @@ from lxml.html import builder as E
 from .slate2html import slate_to_html, elements_to_text, slate_to_elements
 
 
+TABLE_CELLS = {"header": E.TH, "data": E.TD}
+
+
 def nop_converter(block_data):
     return None
 
 
 def convert_slate(block_data):
-    value = slate_to_html(block_data["value"])
-    return value
-
-
-TABLE_CELLS = {"header": E.TH, "data": E.TD}
+    return slate_to_elements(block_data["value"])
 
 
 def convert_slate_table(block_data):
@@ -34,7 +33,36 @@ def convert_slate_table(block_data):
         children.append(erow)
 
     etable = E.TABLE(*children, **attributes)
-    return elements_to_text([etable])
+    return [etable]
+
+
+def iterate_blocks(data):
+    uids = data["blocks_layout"]["items"]
+    blocks = data["blocks"]
+
+    for uid in uids:
+        yield (uid, blocks[uid])
+
+
+def convert_columns_block(block_data):
+    _type = block_data.pop("@type")
+    data = block_data.pop("data")
+    attributes = {
+        "data-block-type": _type,
+        "data-volto-columnsBlock": json.dumps(block_data),
+    }
+
+    children = []
+    for _, coldata in iterate_blocks(data):
+        colelements = []
+        for _, block in iterate_blocks(coldata):
+            colelements.extend(convert_block_to_elements(block))
+        column = E.DIV(*colelements)
+        children.append(column)
+
+    div = E.DIV(*children, **attributes)
+
+    return [div]
 
 
 def convert_quote(block_data):
@@ -46,7 +74,7 @@ def convert_quote(block_data):
     }
     children = slate_to_elements(value)
     div = E.DIV(*children, **attributes)
-    return elements_to_text([div])
+    return [div]
 
 
 def convert_image(block_data):
@@ -55,7 +83,7 @@ def convert_image(block_data):
         "src": block_data["url"],
         "data-volto-block": json.dumps(block_data),
     }
-    return elements_to_text([E.IMG(**attributes)])
+    return [E.IMG(**attributes)]
 
 
 converters = {
@@ -64,10 +92,11 @@ converters = {
     "title": nop_converter,
     "quote": convert_quote,
     "image": convert_image,
+    "columnsBlock": convert_columns_block,
 }
 
 
-def convert_block_to_html(block_data):
+def convert_block_to_elements(block_data):
     _type = block_data.get("@type", None)
 
     if _type is None:
@@ -81,15 +110,15 @@ def convert_block_to_html(block_data):
 
 
 def convert_blocks_to_html(data):
-    # __import__("pdb").set_trace()
     order = data.blocks_layout["items"]
     blocks = data.blocks
     fragments = []
 
     for uid in order:
         block = blocks[uid]
-        data = convert_block_to_html(block)
-        if data:
-            fragments.append(data)
+        elements = convert_block_to_elements(block)
+        if elements:
+            html = elements_to_text(elements)
+            fragments.append(html)
 
     return "\n".join(fragments)
