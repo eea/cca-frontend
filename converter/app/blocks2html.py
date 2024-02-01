b52158b807eee4492c7837f61d986fadc4d0ea97
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2024-02-01 19:01:10 +0200

    wip on convert blocks to html

--- converter/app/blocks2html.py
+++ converter/app/blocks2html.py
@@ -1,7 +1,25 @@
-from .slate2html import slate_to_html
+from lxml.html import builder as E
+from .slate2html import slate_to_html, elements_to_text, slate_to_elements
 
 
-converters = {"slate": slate_to_html}
+def convert_slate(block_data):
+    value = slate_to_html(block_data["value"])
+    return value
+
+
+def nop_converter(*args):
+    return
+
+
+def convert_quote(block_data):
+    attributes = {"data-block_type": "quote"}
+    children = slate_to_elements(block_data["value"])
+    div = E.DIV(*children, **attributes)
+    return elements_to_text([div])
+
+
+converters = {"slate": convert_slate,
+              "title": nop_converter, "quote": convert_quote}
 
 
 def convert_block_to_html(block_data):
@@ -10,11 +28,15 @@ def convert_block_to_html(block_data):
     if _type is None:
         raise ValueError
 
+    if _type not in converters:
+        print(f"{_type} has no block handler")
+        return ""
+
     return converters[_type](block_data)
 
 
 def convert_blocks_to_html(data):
-    __import__("pdb").set_trace()
+    # __import__("pdb").set_trace()
     order = data.blocks_layout["items"]
     blocks = data.blocks
     fragments = []
@@ -22,6 +44,7 @@ def convert_blocks_to_html(data):
     for uid in order:
         block = blocks[uid]
         data = convert_block_to_html(block)
-        fragments.append(data)
+        if data:
+            fragments.append(data)
 
     return "\n".join(fragments)
